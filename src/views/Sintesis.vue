e6a94d2e3033f65f29b84af4670fd09b7b5a7aff
cveimarsena <vcelis@sena.edu.co>
2026-07-14 23:26:16 -0500

    Avance1

--- src/views/Sintesis.vue
+++ src/views/Sintesis.vue
@@ -6,7 +6,7 @@
       | El componente formativo aborda el proceso de autoevaluación en acreditación en salud, destacando su importancia como herramienta para valorar el cumplimiento de los estándares de calidad mediante la aplicación de metodologías, herramientas y técnicas para la recolección, análisis e interpretación de información cualitativa y cuantitativa. Asimismo, desarrolla los fundamentos de la calificación de los resultados, incluyendo los criterios de valoración, los rangos, las puntuaciones y las medidas de tendencia central como apoyo para la toma de decisiones. Finalmente, presenta el plan de mejoramiento como un instrumento de gestión orientado a la formulación, registro, implementación y seguimiento de acciones que contribuyen al fortalecimiento de la calidad institucional y al mejoramiento continuo de los servicios de salud.
 
     .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+      .col-lg-12.mb-5.bgs.p-4.brad
         figure
           img(src="@/assets/curso/sintesis.svg", alt="Síntesis del componente formativo sobre autoevaluación, calificación y mejoramiento en acreditación en salud.")
       .col-auto
